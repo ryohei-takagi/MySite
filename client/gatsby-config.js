90ef318f3960444eb58d684ca3126dfc6b80cfd9
RyoheiTakagi <r.t.flatspace@gmail.com>
2022-03-18 20:28:17 +0900

    add Google Analytics

--- client/gatsby-config.js
+++ client/gatsby-config.js
@@ -4,12 +4,25 @@ module.exports = {
       title: `Ryohei Takagi's Profile site`,
     siteUrl: `https://ryohei-takagi.me`
   },
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sharp", "gatsby-transformer-sharp",
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "images",
+        "path": "./src/images/"
+      },
+      __key: "images"
     },
-    __key: "images"
-  }]
+    {
+      resolve: 'gatsby-plugin-google-gtag',
+      options: {
+        trackingIds: [
+          "G-2VYV2GCB8R",
+        ],
+        pluginConfig: {
+          head: true,
+        }
+      }
+    }
+  ]
 };
